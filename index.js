efdaf69b88ef44e0c3a6b6e5661fb56509cdd35a
Daniel Caldas <daniel.caldasgoncalves@gmail.com>
2022-08-22 17:30:25 -0300

    cors novamnete atualizado :/

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ const db = mysql.createPool({
 app.use(cors({
     origin:"*",
 }))
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin","*")
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With,Content-Type, Accept")
+    next();
+})
+
 app.use(express.json())
 
 
