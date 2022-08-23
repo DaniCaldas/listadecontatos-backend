cb6d536e7d8aabafd7ecc6809b3795f56ccf15bb
Daniel Caldas <daniel.caldasgoncalves@gmail.com>
2022-08-23 18:09:17 -0300

    :/

--- index.js
+++ index.js
@@ -98,6 +98,6 @@ app.get('/test', (req,res) => {
     res.send("servidor rodando!")
 }) 
 
-app.listen(process.env.PORT ||3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log("servidor rodando")
 })
